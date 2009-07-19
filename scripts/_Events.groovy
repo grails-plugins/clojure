d678a722620270cd05cf744c5896721f1f751ce6
brownj <brownj@832c1c66-4827-0410-8465-ccb17913cd09>
2009-07-19 05:04:07 +0000

    fix dest dir when copying src files
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-clojure/trunk@54256 832c1c66-4827-0410-8465-ccb17913cd09

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,5 +1,5 @@
 eventCompileStart = {
-    def cljDestDir = "${grailsSettings.projectWarExplodedDir}/WEB-INF/grails-app/clj"
+    def cljDestDir = "${grailsSettings.projectWarExplodedDir}/WEB-INF/clj"
     ant.mkdir dir: cljDestDir
     ant.copy (todir: cljDestDir) {
         fileset(dir:"${basedir}/src/clj", includes:"*.clj")
