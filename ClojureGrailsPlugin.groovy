860d02ec337ad43d9a8a4077deefff1d09fa5b8c
brownj <brownj@832c1c66-4827-0410-8465-ccb17913cd09>
2009-07-19 21:48:19 +0000

    fix pluginExcludes to exclude src/clj instead of grails-app/clj
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-clojure/trunk@54257 832c1c66-4827-0410-8465-ccb17913cd09

--- ClojureGrailsPlugin.groovy
+++ ClojureGrailsPlugin.groovy
@@ -10,7 +10,7 @@ class ClojureGrailsPlugin {
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
-            "grails-app/clj/*",
+            "src/clj/*",
             "grails-app/controllers/*"
     ]
     
