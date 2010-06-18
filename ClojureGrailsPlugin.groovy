90abd28f31f0beeea1a67807713eb827ca1240fb
Jeff Brown <jeff@jeffandbetsy.net>
2010-06-18 07:18:44 -0500

    require Grails 1.2 or greater

--- ClojureGrailsPlugin.groovy
+++ ClojureGrailsPlugin.groovy
@@ -20,7 +20,7 @@ class ClojureGrailsPlugin {
     // the plugin version
     def version = "0.5-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.1.1 > *"
+    def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
